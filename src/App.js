bee4c318f071554d9af8464f4770915f14108cc6
Jennifer Pazos <jmp521@gmail.com>
2018-07-07 00:35:15 -0500

    added theme template and updated basic css to center chatbot

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-
+// import ChatBot from 'react-simple-chatbot';
+import MainTheme from './components/theme.js'
 import './App.css';
-import Chatbot from './components/chatbot.js'
 
 class App extends Component {
+
   render() {
+    // const steps = [
+    //   {
+    //     id: '1',
+    //     message: 'Hello World',
+    //     end: true,
+    //   },
+    // ];
     return (
       <div className="App">
-        <Chatbot />
+        <div className="header">
+          <h1>Welcome to my ChatBot</h1>
+          <h4>Created with the help of react-simple-chatbot</h4>
+        </div>
+        <div className="mainTheme">
+          <MainTheme />
+        </div>
       </div>
     );
   }
